6a80ca7307df52911983259e3df135c88b951ef5
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-07 19:13:12 +0530

    queue with max

--- epi_judge_java/epi/stackandqueue/QueueWithMax.java
+++ epi_judge_java/epi/stackandqueue/QueueWithMax.java
@@ -1,25 +1,59 @@
-package epi;
+package epi.stackandqueue;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class QueueWithMax {
+
+  // queue to store queue elements
+  private Queue<Integer> queue = new LinkedList<>();
+
+  // doubly linked list to store max elements
+  // current maximum will be at start of the list
+  // deque will contain elems which can be max in desc order
+  private Deque<Integer> deque = new LinkedList<>();
+
   public void enqueue(Integer x) {
-    // TODO - you fill in here.
-    return;
+
+    // add to the queue
+    queue.add(x);
+
+    // remove all the elements less than this from the back of the deque
+    // because they can never become max as current elem will be there in the queue
+    // until those elems are removed
+    while (!deque.isEmpty() &&  deque.peekLast() < x){
+      deque.removeLast();
+    }
+
+    // add current element to end of deque
+    deque.addLast(x);
+
   }
+
   public Integer dequeue() {
-    // TODO - you fill in here.
-    return 0;
+
+    if(queue.isEmpty()) throw new IllegalStateException("queue is empty");
+
+    int elem = queue.poll();
+
+    // if max is removed then remove max from head of deque
+    if(deque.peekFirst().equals(elem)){
+      deque.removeFirst();
+    }
+    return elem;
   }
+
   public Integer max() {
-    // TODO - you fill in here.
-    return 0;
+
+    if(queue.isEmpty()) throw new IllegalStateException("queue is empty");
+
+    // head in the deque is the current max element
+    return deque.peekFirst();
   }
+
   @EpiUserType(ctorParams = {String.class, int.class})
   public static class QueueOp {
     public String op;
