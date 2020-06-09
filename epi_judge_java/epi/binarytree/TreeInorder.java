e0f6a72a37fa1c451d0cf3af10c60613e8fcbb0f
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-09 11:11:41 +0530

    inorder without recursion

--- epi_judge_java/epi/binarytree/TreeInorder.java
+++ epi_judge_java/epi/binarytree/TreeInorder.java
@@ -1,8 +1,11 @@
-package epi;
+package epi.binarytree;
+import epi.BinaryTree;
+import epi.BinaryTreeNode;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
+
 public class TreeInorder {
 
   private static class NodeAndState {
@@ -18,8 +21,28 @@ public class TreeInorder {
 
   @EpiTest(testDataFile = "tree_inorder.tsv")
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+
+    List<Integer> result = new ArrayList<>();
+
+    // use an explicit stack
+    Deque<BinaryTreeNode<Integer>> stack = new LinkedList<>();
+    BinaryTreeNode<Integer> curr = tree;
+
+    while (!stack.isEmpty() || curr != null){
+      // if there a left to the curr add that to the stack
+      if(curr != null){
+        stack.push(curr);
+        curr = curr.left;
+      }else {
+        // all the left nodes are completed until this curr
+        curr = stack.pop();
+        result.add(curr.data);
+        // move to right
+        curr = curr.right;
+      }
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
