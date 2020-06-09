6b78270b3298f59feaae2c20a4997586ecd9651a
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-09 13:32:21 +0530

    successor of node in binary tree

--- epi_judge_java/epi/binarytree/SuccessorInTree.java
+++ epi_judge_java/epi/binarytree/SuccessorInTree.java
@@ -1,4 +1,5 @@
-package epi;
+package epi.binarytree;
+import epi.BinaryTree;
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
@@ -6,9 +7,23 @@ import epi.test_framework.TimedExecutor;
 public class SuccessorInTree {
 
   public static BinaryTree<Integer> findSuccessor(BinaryTree<Integer> node) {
-    // TODO - you fill in here.
-    return null;
+
+    // if node has right sub tree then succesor is left most node in that subree
+    if(node.right != null){
+      BinaryTree<Integer> succ = node.right;
+      while (succ.left != null){
+        succ = succ.left;
+      }
+      return succ;
+    }
+
+    // if the node does not have right child then move towards root until u make right turn
+    while (node.parent != null && node.parent.left != node){
+      node = node.parent;
+    }
+    return node.parent;
   }
+
   @EpiTest(testDataFile = "successor_in_tree.tsv")
   public static int findSuccessorWrapper(TimedExecutor executor,
                                          BinaryTree<Integer> tree, int nodeIdx)
