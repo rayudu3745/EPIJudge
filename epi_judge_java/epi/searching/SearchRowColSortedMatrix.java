d3d85a007681190c89befd92afb21ae70a9788ad
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-15 20:13:49 +0530

    search in sorted matrix

--- epi_judge_java/epi/searching/SearchRowColSortedMatrix.java
+++ epi_judge_java/epi/searching/SearchRowColSortedMatrix.java
@@ -1,4 +1,4 @@
-package epi;
+package epi.searching;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
@@ -7,8 +7,24 @@ public class SearchRowColSortedMatrix {
   @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
 
   public static boolean matrixSearch(List<List<Integer>> A, int x) {
-    // TODO - you fill in here.
-    return true;
+
+    int row = 0, col = A.get(0).size() - 1;
+
+    // compare x with top right corner number
+
+    while (row < A.size() && col >=0){
+      if(A.get(row).get(col) > x){
+      // it cant be present in that col
+        col--;
+      }else if(A.get(row).get(col) < x){
+        // it can't be present in that row
+        row++;
+      }else {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   public static void main(String[] args) {
