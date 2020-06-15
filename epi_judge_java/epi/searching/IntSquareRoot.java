d3ce94c7e038d5bf50014bcaa53bf47447f1bf24
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-15 14:32:04 +0530

    int square root

--- epi_judge_java/epi/searching/IntSquareRoot.java
+++ epi_judge_java/epi/searching/IntSquareRoot.java
@@ -1,12 +1,27 @@
-package epi;
+package epi.searching;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IntSquareRoot {
   @EpiTest(testDataFile = "int_square_root.tsv")
 
   public static int squareRoot(int k) {
-    // TODO - you fill in here.
-    return 0;
+
+    long L = 0, U = k;
+    while (L <= U){
+
+      long M = L + (U - L) / 2;
+
+      long square = M * M;
+
+      if(square <= k){
+        L = M + 1;
+      }else {
+        U = M - 1;
+      }
+
+    }
+
+    return (int)L - 1;
   }
 
   public static void main(String[] args) {
