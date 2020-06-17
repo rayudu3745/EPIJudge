dd9c49a89c49f927c486a02a83d9af24044cb009
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-17 19:26:51 +0530

    anagrams

--- epi_judge_java/epi/hashtables/Anagrams.java
+++ epi_judge_java/epi/hashtables/Anagrams.java
@@ -1,18 +1,29 @@
-package epi;
+package epi.hashtables;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
+
 public class Anagrams {
   @EpiTest(testDataFile = "anagrams.tsv")
 
   public static List<List<String>> findAnagrams(List<String> dictionary) {
-    // TODO - you fill in here.
-    return null;
+
+    // group strings by their sorted order ;
+    Map<String, List<String>> anagrams = dictionary.stream()
+            .collect(Collectors.groupingBy((s) -> {
+                              char[] chars = s.toCharArray();
+                              Arrays.sort(chars);
+                              return new String(chars);
+    }));
+    return anagrams.values().stream().filter(list -> list.size() > 1).collect(Collectors.toList());
   }
   @EpiTestComparator
   public static boolean comp(List<List<String>> expected,
