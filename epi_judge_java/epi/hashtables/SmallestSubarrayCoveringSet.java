12ca0cc8c1af71b32f32aff08f0f33d567bb6e4e
Lakshmi Abbireddy <Lakshmi.Abbireddy@walmartlabs.com>
2020-06-21 13:06:27 +0530

    smallest subarray covering set

--- epi_judge_java/epi/hashtables/SmallestSubarrayCoveringSet.java
+++ epi_judge_java/epi/hashtables/SmallestSubarrayCoveringSet.java
@@ -1,12 +1,11 @@
-package epi;
+package epi.hashtables;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 public class SmallestSubarrayCoveringSet {
 
   // Represent subarray by starting and ending indices, inclusive.
@@ -22,9 +21,49 @@ public class SmallestSubarrayCoveringSet {
 
   public static Subarray findSmallestSubarrayCoveringSet(List<String> paragraph,
                                                          Set<String> keywords) {
-    // TODO - you fill in here.
-    return new Subarray(0, 0);
+
+    Subarray result = new Subarray(-1,-1);
+
+    Map<String, Integer> keyWordFreq = new HashMap<>();
+
+    int numKeywords = keywords.size();
+
+    // slide the window to contain the all the keywords
+    // starting with [0,0]
+    for (int left = 0, right = 0 ; right < paragraph.size() ; right++){
+
+      // expand right to contain all the keywords
+      String currRight = paragraph.get(right);
+      if (keywords.contains(currRight)){
+        keyWordFreq.put(currRight, keyWordFreq.get(currRight) == null ? 1 : keyWordFreq.get(currRight) + 1);
+      }
+
+      // loop enters when all the keywords are present int the current
+      // contract left until the all the keywords are still present
+      while (keyWordFreq.size() == numKeywords){
+        String currLeft = paragraph.get(left);
+
+        if (keywords.contains(currLeft)){
+          // remove from the map if its the last available so that loop exists
+          if (keyWordFreq.get(currLeft).equals(1)){
+            // now check if current sub array is the smallest
+            if((result.start == -1 && result.end == -1) || (right - left) < (result.end - result.start)){
+              result = new Subarray(left, right);
+            }
+            keyWordFreq.remove(currLeft);
+          }else {
+            // else reduce the frequcency
+            keyWordFreq.put(currLeft, keyWordFreq.get(currLeft) - 1);
+          }
+        }
+        left++;
+      }
+
+    }
+
+    return result;
   }
+
   @EpiTest(testDataFile = "smallest_subarray_covering_set.tsv")
   public static int findSmallestSubarrayCoveringSetWrapper(
       TimedExecutor executor, List<String> paragraph, Set<String> keywords)
